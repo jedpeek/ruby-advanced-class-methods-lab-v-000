2479935598a331b7779c3c2a43aafd2eb20ac0ab
Jed Peek <jedpeek@yahoo.com>
2018-06-09 20:24:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,8 +41,8 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    find_by_name(name)
     create_by_name(name) if !!@@all.include?(name) == false
+    find_by_name(name)
 end
 
   def self.alphabetical
